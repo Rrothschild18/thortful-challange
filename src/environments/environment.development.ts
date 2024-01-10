392a4a270fff7b79aa6aea39be636b47988cbd3d
rrothschild18 <raultonello18@gmail.com>
2024-01-10 16:34:51 -0300

    feat: login component

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,12 @@
 export const environment = {
   clientId: '1e2ebb58c1e349d1a776117b1ff97fe9',
-  authURL: 'https://accounts.spotify.com/api/token',
+  authURL: 'https://accounts.spotify.com/authorize',
+  redirectURL: 'http://localhost:4200/home',
+  scopes: [
+    'user-read-recently-played',
+    'user-top-read',
+    'user-library-read',
+    'playlist-read-private',
+    'playlist-read-collaborative',
+  ],
 };
