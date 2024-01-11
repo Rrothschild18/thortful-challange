2f8cc54e0af2bb9193c17cd067fcd7328b02ef59
rrothschild18 <raultonello18@gmail.com>
2024-01-11 09:26:30 -0300

    feat: create an callback cmp to be redirected from spotify

---
+++ src/app/auth/callback/callback.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-callback',
+  standalone: true,
+  imports: [],
+  template: ``,
+  styles: [``],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CallbackComponent {}
