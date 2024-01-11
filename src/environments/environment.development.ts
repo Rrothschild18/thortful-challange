3b12c0f1c8333fe05fd8fe8f0277aef9f7764f16
rrothschild18 <raultonello18@gmail.com>
2024-01-11 09:28:55 -0300

    refactor: changes redirect URL

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   clientId: '1e2ebb58c1e349d1a776117b1ff97fe9',
   authURL: 'https://accounts.spotify.com/authorize',
-  redirectURL: 'http://localhost:4200/home',
+  redirectURL: 'http://localhost:4200/auth/callback',
   scopes: [
     'user-read-recently-played',
     'user-top-read',
