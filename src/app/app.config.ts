dbd1ee6eb93335aeb5c7d6df5f426292a07567c8
rrothschild18 <raultonello18@gmail.com>
2024-01-11 09:31:33 -0300

    feat: general adds to a simple authentication

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,18 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { NgxsModule } from '@ngxs/store';
 import { routes } from './app.routes';
 import { LayoutState } from './store/layout/layout.model';
+import { UserState } from './store/user/user.state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
-    importProvidersFrom(NgxsModule.forRoot([LayoutState])),
+    importProvidersFrom(NgxsModule.forRoot([LayoutState, UserState])),
+    provideHttpClient(),
   ],
 };
