d20824c355573d5b2450f4945fbbd5f05322dfd3
rrothschild18 <raultonello18@gmail.com>
2024-01-11 21:53:17 -0300

    refactor: home now use store selectors

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,13 @@
 import { AsyncPipe, JsonPipe, TitleCasePipe } from '@angular/common';
-import { HttpStatusCode } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { Observable, catchError, map, of } from 'rxjs';
-import { TopItemsList } from '../shared/models/user.model';
-import { UserService } from './../shared/services/user.service';
-import { Genres } from './services/genres.model';
-import { GenresService } from './services/genres.service';
+import { RouterModule } from '@angular/router';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { Artist } from '../shared/models/artist.model';
+import { Home } from '../store/home/home.actions';
+import { HomeState } from '../store/home/home.state';
 
 @Component({
   selector: 'app-home',
@@ -18,6 +18,7 @@ import { GenresService } from './services/genres.service';
     MatChipsModule,
     TitleCasePipe,
     MatGridListModule,
+    RouterModule,
   ],
   template: `
     <section class="Home">
@@ -32,20 +33,34 @@ import { GenresService } from './services/genres.service';
 
         <h1>Favorite artists</h1>
         <div class="Scroll d-flex flex-nowrap mt-10">
-          @if (topItems$ | async; as topItems) {
-            @for (artist of topItems.items; track artist.uri) {
-              <div class="Artist me-4">
-                <div class="content">
-                  <img src="{{ artist.images[0].url }}" />
+          @if (artists$ | async; as artists) {
+            @for (artist of artists; track artist.uri) {
+              <a
+                [routerLink]="[
+                  '/home',
+                  {
+                    outlets: {
+                      overview: ['artist-overview', { id: artist.id }]
+                    }
+                  }
+                ]"
+                (click)="selectCurrentArtist(artist.id)"
+              >
+                <div class="Artist me-4">
+                  <div class="content">
+                    <img src="{{ artist.images[0].url }}" />
 
-                  <div class="d-flex flex-column align-items-center">
-                    <p class="text-white fw-bold m-0 mt-2">{{ artist.name }}</p>
-                    <small class="text-gray fw-bold m-0"
-                      >1.1 mi followers</small
-                    >
+                    <div class="d-flex flex-column align-items-center">
+                      <p class="text-white fw-bold m-0 mt-2">
+                        {{ artist.name }}
+                      </p>
+                      <small class="text-gray fw-bold m-0"
+                        >1.1 mi followers</small
+                      >
+                    </div>
                   </div>
                 </div>
-              </div>
+              </a>
             }
           }
         </div>
@@ -110,52 +125,19 @@ import { GenresService } from './services/genres.service';
   ],
 })
 export class HomeComponent implements OnInit {
-  private genresService = inject(GenresService);
-  private userService = inject(UserService);
+  readonly #store = inject(Store);
 
+  @Select(HomeState.genres)
   genres$!: Observable<string[]>;
-  topItems$!: Observable<TopItemsList>;
+
+  @Select(HomeState.topArtists)
+  artists$!: Observable<Artist[]>;
 
   ngOnInit(): void {
-    this.fetchGenres();
-    this.fetchUserTopItems();
+    this.#store.dispatch(new Home.FirstLoad());
   }
 
-  fetchGenres() {
-    this.genres$ = this.genresService.getGenres().pipe(
-      map(
-        ({ genres }: { genres: Genres[] }) =>
-          genres.slice(0, 11) as unknown as string[], //TitleCasePipe only accepts strings inputs
-      ),
-      catchError((err) => {
-        console.log({ err });
-        if (err.status === HttpStatusCode.TooManyRequests)
-          //Remove
-          return of([
-            'acoustic',
-            'afrobeat',
-            'alt-rock',
-            'alternative',
-            'ambient',
-            'anime',
-            'black-metal',
-            'bluegrass',
-            'blues',
-            'bossanova',
-            'brazil',
-          ]);
-
-        return of([]);
-      }),
-    );
-  }
-
-  fetchUserTopItems() {
-    this.topItems$ = this.userService.getUserTopItems({
-      limit: 10,
-      offset: 5,
-      type: 'artist',
-      time_range: 'medium_term',
-    });
+  selectCurrentArtist(id: string) {
+    this.#store.dispatch(new Home.SetCurrentSelectedArtistId(id));
   }
 }
