9a03d660f4c6da411235d34ca9d9dd608adb18a5
rrothschild18 <raultonello18@gmail.com>
2024-01-11 21:51:41 -0300

    refactor: add env variable to services

--- src/app/home/services/genres.service.ts
+++ src/app/home/services/genres.service.ts
@@ -1,14 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 import { GenresFromApi } from './genres.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GenresService {
-  #genresURL: string =
-    'https://api.spotify.com/v1/recommendations/available-genre-seeds';
+  readonly #genresURL: string = `${environment.baseURL}/recommendations/available-genre-seeds`;
 
   constructor(private http: HttpClient) {}
 
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,15 +1,16 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 import { TopItemsList, UserTopItemsParams } from './../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  #topItemsURL: string = 'https://api.spotify.com/v1/me/top';
-  #profileURL: string = 'https://api.spotify.com/v1/users/{user_id}';
-  #followingURL: string = 'https://api.spotify.com/v1/me/following';
+  readonly #topItemsURL: string = `${environment.baseURL}/me/top`;
+  readonly #profileURL: string = `${environment.baseURL}/users/{user_id}`;
+  readonly #followingURL: string = `${environment.baseURL}/me/following`;
 
   constructor(private http: HttpClient) {}
 
