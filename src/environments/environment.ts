602149486686c2035a7548f09c2fceb12f4c507e
rrothschild18 <raultonello18@gmail.com>
2024-01-15 10:04:42 -0300

    chore: add env for prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,13 @@
-export const environment = {};
+export const environment = {
+  baseURL: 'https://api.spotify.com/v1',
+  clientId: '1e2ebb58c1e349d1a776117b1ff97fe9',
+  authURL: 'https://accounts.spotify.com/authorize',
+  redirectURL: 'https://thortful-challange-ljd4.vercel.app/auth/callback',
+  scopes: [
+    'user-read-recently-played',
+    'user-top-read',
+    'user-library-read',
+    'playlist-read-private',
+    'playlist-read-collaborative',
+  ],
+};
