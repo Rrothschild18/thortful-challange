80d0ac7b22fda267f187b0dfd96e3ed13dc3c8a4
rrothschild18 <raultonello18@gmail.com>
2024-01-15 10:58:24 -0300

    fix: fixed artist-view bug withou ids

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
 })
 export class AppComponent implements OnInit {
   title = 'thortful-challange';
+  localStorage: Storage = window.localStorage;
 
   #router = inject(Router);
   isLoginRoute$!: Observable<boolean>;
@@ -22,7 +23,15 @@ export class AppComponent implements OnInit {
     this.hideMenuByUrl();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const ids = this.localStorage.getItem('favoriteArtistsIds')! ?? [];
+    const parsedIds = JSON.parse(ids);
+    debugger;
+
+    if (!parsedIds.length) {
+      this.localStorage.setItem('favoriteArtistsIds', JSON.stringify([]));
+    }
+  }
 
   hideMenuByUrl() {
     this.isLoginRoute$ = this.#router.events.pipe(
@@ -32,7 +41,7 @@ export class AppComponent implements OnInit {
         const paths = ['/auth', '/not-found'];
 
         return !paths.includes(routers.url);
-      }),
+      })
     );
   }
 }
